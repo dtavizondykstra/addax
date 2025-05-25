39e4d07b287f65be9fca4bacc8f6081b205a2034
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2025-05-24 20:48:00 -0600

    build: Refactor __init__.py to improve module structure and readability

--- src/addax/__init__.py
+++ src/addax/__init__.py
@@ -1,3 +1,27 @@
+# src/addax/__init__.py
+"""Addax: A DataFrame Processing Library"""
 # read version from installed package
 from importlib.metadata import version
+
 __version__ = version("addax")
+
+
+from addax.addax import (
+    read_csv,
+    standardize_headers,
+    format_text_data_target,
+    remove_rows_missing_target,
+    analyze_sentiment_text,
+    analyze_sentiment_dataframe,
+    process_text_column,
+)
+
+__all__ = [
+    "read_csv",
+    "standardize_headers",
+    "format_text_data_target",
+    "remove_rows_missing_target",
+    "analyze_sentiment_text",
+    "analyze_sentiment_dataframe",
+    "process_text_column",
+]
