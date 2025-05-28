02493420db3c5199ffddd39d33705d1568d408b3
Dalia Tavizon-Dykstra <83558848+dtavizondykstra@users.noreply.github.com>
2025-05-28 07:42:33 -0600

    refactor: Update imports and __all__ to improve module structure

--- src/addax/__init__.py
+++ src/addax/__init__.py
@@ -8,20 +8,33 @@ __version__ = version("addax")
 
 from addax.addax import (
     read_csv,
+    normalize_column_name,
+    normalize_series_text,
+    standardize_target_col_name,
     standardize_headers,
-    format_text_data_target,
+    standardize_target_col_data,
     remove_rows_missing_target,
     analyze_sentiment_text,
     analyze_sentiment_dataframe,
-    process_text_column,
+    process_sentiment,
+    label_polarity,
+    label_subjectivity,
+    logger,
 )
 
 __all__ = [
     "read_csv",
+    "normalize_column_name",
+    "normalize_series_text",
+    "standardize_target_col_name",
     "standardize_headers",
-    "format_text_data_target",
+    "standardize_target_col_data",
     "remove_rows_missing_target",
     "analyze_sentiment_text",
     "analyze_sentiment_dataframe",
-    "process_text_column",
+    "process_sentiment",
+    "label_polarity",
+    "label_subjectivity",
+    "logger",
+    "__version__",
 ]
